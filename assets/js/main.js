b9dec26f7d8ae2b191adddfbaced17413802514a
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2022-10-06 21:42:21 -0300

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const inputQuantity = document.querySelector("#quantity");
 const inputColor = document.querySelector("#color");
-const price = document.querySelector("#price");
+const price = 70000;
 
 const btn = document.querySelector("#btn");
 
@@ -9,8 +9,7 @@ const totalQuantity = document.querySelector("#totalQuantity");
 const selectedColor = document.querySelector("#selectedColor");
 
 btn.addEventListener("click", () => {
-  console.log("asdasd")
 totalQuantity.textContent = inputQuantity.value;
-totalPrice.textContent = inputQuantity.value * price.textContent;
+totalPrice.textContent = inputQuantity.value * price;
 selectedColor.style.backgroundColor = inputColor.value;
 })
